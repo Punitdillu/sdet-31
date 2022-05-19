e68aa71f82f7f0a38f2fa198479391bfd2a6ebcb
Punitdillu <punitranjan2105@gmail.com>
2022-05-19 23:28:21 +0530

    3rd commit

--- com.crm.sdet31/src/main/java/com/vtigerObjectRepository/CampaignInformationPage.java
+++ com.crm.sdet31/src/main/java/com/vtigerObjectRepository/CampaignInformationPage.java
@@ -24,6 +24,7 @@ public class CampaignInformationPage {
 	
 	public String getheaderText() {
 		return headerText.getText();
+		
 	}
 
 
--- com.crm.sdet31/src/test/java/com/Array_Practice/Bubble_shorting_ascending.java
+++ com.crm.sdet31/src/test/java/com/Array_Practice/Bubble_shorting_ascending.java
@@ -21,6 +21,7 @@ public class Bubble_shorting_ascending {
 		for (int i = 0; i < a.length; i++) {
 			
 			System.out.print(a[i]+" ");
+			System.out.println(" ");
 		
 		}
 }}
